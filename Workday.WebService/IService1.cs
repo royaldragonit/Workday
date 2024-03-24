dcb454e4ec3887c731760403321425f8a3a6d7da
Hoang Long <royaldragonit@gmail.com>
2024-03-24 13:01:23 +0700

    Initial Project

---
+++ Workday.WebService/IService1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Workday.WebService.WorkdayReference;
+
+namespace Workday.WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        Get_Applicants_ResponseType Get_Applicants(Workday_Common_HeaderType Workday_Common_Header, Get_Applicants_RequestType Get_Applicants_Request);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "Workday.WebService.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ Workday.WebService/Service1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Workday.WebService.WorkdayReference;
+
+namespace Workday.WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public Get_Applicants_ResponseType Get_Applicants(Workday_Common_HeaderType Workday_Common_Header, Get_Applicants_RequestType Get_Applicants_Request)
+        {
+            // Tạo một phiên bản của lớp RecruitingPortClient
+            RecruitingPortClient client = new RecruitingPortClient();
+
+            try
+            {
+                // Gọi các phương thức API từ dịch vụ WCF
+                var result = client.Get_Applicants(Workday_Common_Header, Get_Applicants_Request);
+
+                // Xử lý kết quả ở đây
+
+                // Đóng kết nối
+                client.Close();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // Xử lý ngoại lệ
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            return null;
+        }
+    }
+}
